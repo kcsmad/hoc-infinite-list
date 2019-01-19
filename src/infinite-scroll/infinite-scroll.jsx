19a83b7848827b9b1803a907b21110ad74c5df07
Kevin Maduro <nivekzin@gmail.com>
2019-01-19 01:57:32 -0200

    Fixed CSS and added index relative that should fix active item

--- src/infinite-scroll/infinite-scroll.jsx
+++ src/infinite-scroll/infinite-scroll.jsx
@@ -5,50 +5,50 @@ import animesMock from './__mocks__/animes';
 import './styles/infinite-scroll.scss';
 
 export default class InfiniteScroll extends Component {
-  
+
   constructor(props) {
     super(props);
-    
+
     this.state = {
       activeIndex: 0,
-      slidesToShow: 3,
+      threshold: 3,
     };
-  
+
     this.handleChangeActivePosition = this.handleChangeActivePosition.bind(this);
     this.handleChangeIndex = this.handleChangeIndex.bind(this);
   }
-  
+
   componentWillMount() {
     document.addEventListener('keydown', this.handleNavigation.bind(this));
     this.animes = animesMock;
   }
-  
+
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleNavigation.bind(this));
   }
-  
+
   handleNavigation(e) {
     e.stopPropagation();
-    
+
     if (e.key === 'ArrowLeft') {
       this.handleChangeActivePosition('Left');
     }
-  
+
     if (e.key === 'ArrowRight') {
       this.handleChangeActivePosition('Right');
     }
   }
-  
+
   handleChangeActivePosition(direction) {
     const { activeIndex } = this.state;
-    
+
     const indexChanged = direction === 'Left'
       ? this.handleChangeIndex(activeIndex - 1, this.animes)
       : this.handleChangeIndex(activeIndex + 1, this.animes);
-    
+
     this.setState({ activeIndex: indexChanged });
   }
-  
+
   handleChangeIndex(index, array) {
     const indexModulus = index % array.length;
     return indexModulus < 0
@@ -56,16 +56,32 @@ export default class InfiniteScroll extends Component {
     : indexModulus;
   }
 
+  handleRelativeIndex(relativeIndex, threshold) {
+    const indexArray = [];
+    for (let i = relativeIndex - threshold; i <= relativeIndex + threshold; i++) {
+      indexArray.push(i);
+    }
+
+    return indexArray;
+  }
+
+  handleCurrentDisplayedItems(array, activeIndex = 0, threshold = 0) {
+    return this.handleRelativeIndex(activeIndex, threshold).map(i => this.handleChangeIndex(i, array));
+  }
+
   renderListItems() {
-    
+
+    const animes = this.handleCurrentDisplayedItems(this.animes, this.state.activeIndex, this.state.threshold);
+    console.log(animes);
+
     return this.animes.map((an, i) => {
-      const classNames = [
-        this.state.activeIndex === i ? 'item-current' : '',
-        this.state.slidesToShow > i ? 'item-show' : 'item-closed'
+      const styles = [
+        animes.indexOf(i) > -1 ? 'item-show' : 'item-closed',
+        animes[0] === i ? 'item-current' : ''
       ];
-      
+
       return (
-        <li key={i} className={classNames.join(' ')}>
+        <li key={i} className={styles.join(' ')}>
           <img alt="anime-cover" src={"images/" + an.cover} />
           <p>{an.title}</p>
         </li>
